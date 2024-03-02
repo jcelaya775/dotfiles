0d56451e1b796cab258daa404caa079d8593b7b1
Jorge Celaya <jcelaya775@gmail.com>
2024-03-02 11:21:47 -0600

    Add qmk_firmware

---
+++ qmk_firmware/keyboards/crkbd/rules.mk
@@ -0,0 +1,12 @@
+# Build Options
+#   change yes to no to disable
+#
+
+DEFAULT_FOLDER = crkbd/rev1
+
+RGBLIGHT_SUPPORTED = yes
+RGB_MATRIX_SUPPORTED = yes
+# COMBO_ENABLE = yes
+# TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+BOOTLOADER = atmel-dfu
